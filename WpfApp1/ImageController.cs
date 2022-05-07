7184ec63ecd9586d273a188a640f897899b9fad1
cary-cyon <kozvlad99@gmail.com>
2022-05-07 20:14:44 +0300

    init

---
+++ WpfApp1/ImageController.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp1
+{
+    internal abstract class ImageController
+    {
+        protected int[,] pic;
+        protected abstract Task Binaryzation();
+
+        public virtual async Task<BitmapSource> CreateBitmapSourceFromGdiBitmap()
+        {
+
+            Bitmap bitmap = new Bitmap(pic.GetLength(0), pic.GetLength(1));
+            await Binaryzation();
+            for (int i = 0; i < pic.GetLength(0); i++)
+            {
+                for (int j = 0; j < pic.GetLength(1); j++)
+                {
+                    bitmap.SetPixel(i, j, System.Drawing.Color.FromArgb(pic[i, j], pic[i, j], pic[i, j]));
+                }
+            }
+            if (bitmap == null)
+                throw new ArgumentNullException("bitmap");
+
+            var rect = new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height);
+
+            var bitmapData = bitmap.LockBits(
+                rect,
+                ImageLockMode.ReadWrite,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            try
+            {
+                var size = (rect.Width * rect.Height) * 4;
+
+                return BitmapSource.Create(
+                    bitmap.Width,
+                    bitmap.Height,
+                    bitmap.HorizontalResolution,
+                    bitmap.VerticalResolution,
+                    PixelFormats.Bgra32,
+                    null,
+                    bitmapData.Scan0,
+                    size,
+                    bitmapData.Stride);
+            }
+            finally
+            {
+                bitmap.UnlockBits(bitmapData);
+            }
+        }// end CreateBitmapSourceFromGdiBitmap
+    } //end ImageController
+
+    internal class ImageControllerGav : ImageController
+    {
+        private double Level=125;
+        public ImageControllerGav(double Lv, int[,] image )
+        {
+            Level = Lv;
+            pic = (int[,])image.Clone();
+            
+        }
+        protected override async Task Binaryzation()
+        {
+            for (int i = 0; i < pic.GetLength(0); i++)
+            {
+                for (int j = 0; j < pic.GetLength(1); j++)
+                {
+                    pic[i, j] = pic[i, j] < Level ? 0:255;
+                }
+            }
+            
+        } // end Binaryzation
+
+    } // end ImageControllerGav
+
+    internal class ImageControllerOtsu : ImageController
+    {
+        private double Level;
+        public ImageControllerOtsu(int[,] image)
+        {
+            pic = (int[,])image.Clone();
+            double[] gist = new double[256];
+            double size = pic.GetLength(0) * pic.GetLength(1);
+            
+            for(int i = 0; i < pic.GetLength(0); i++)
+            {
+                for(int j = 0; j < pic.GetLength(1); j++)
+                {
+                    double Br = pic[i, j];
+                    gist[(int)Br] += Br / size; 
+                }
+            }
+            double sigma = double.MinValue;
+
+            for(int i =0; i < 256; i++)
+            {
+                double omega1 = 0;
+                double omega2 = 0;
+                double mu1 = 0;
+                double muT = 0;
+                double mu2 = 0;
+                double new_sigma = 0;
+                for (int j = 0; j <= i; j++)
+                {
+                    omega1 += gist[j];
+                }
+                omega2 = 1 - omega1;
+                for (int j = 0; j <= i; j++)
+                {
+                    mu1 += j*gist[j]/omega1;
+                }
+                for (int j = 0; j <= i; j++)
+                {
+                    muT += j * gist[j];
+                }
+                mu2 = (muT - mu1 * omega1) / omega2;
+                new_sigma = omega1 * omega2 * Math.Pow(mu1 - mu2, 2);
+                if (sigma < new_sigma)
+                {
+
+                    sigma = new_sigma;
+                    Level = i;
+                }
+            }
+
+
+        }
+        protected override async Task Binaryzation()
+        {
+            for (int i = 0; i < pic.GetLength(0); i++)
+            {
+                for (int j = 0; j < pic.GetLength(1); j++)
+                {
+                    pic[i, j] = pic[i, j] < Level ? 0 : 255;
+                }
+            }
+
+        } // end Binaryzation
+    } //end Otsu
+    class ImageControllerLocal : ImageController
+    {
+        protected int size;
+        protected override Task Binaryzation()
+        {
+            throw new NotImplementedException();
+        }
+        protected List<double> power(List<double> val)
+        {
+            List<double> ret = new List<double>();
+            foreach(double v in val)
+                ret.Add(v * v);
+            return ret;
+
+        }
+        protected List<double> GetValuesLocal(int i, int j)
+
+        {
+            List<double> values = new List<double>();
+            int start_i = i - (size - 1) / 2;
+            int start_j = j - (size - 1) / 2;
+            int end_i = start_i + size;
+            int end_j = start_j + size;
+
+            if (start_i < 0)
+                start_i = 0;
+            if (end_i > pic.GetLength(0))
+                end_i = pic.GetLength(0);
+            if (start_j < 0)
+                start_j = 0;
+            if (end_j > pic.GetLength(1))
+                end_j = pic.GetLength(1);
+
+            for (int i1 = start_i; i1 < end_i; i1++)
+                for(int j1 = start_j; j1 < end_j; j1++)
+                {
+                    try
+                    {
+                        values.Add(pic[i1, j1]);
+                    }
+                    catch
+                    {
+
+                    }
+                    
+                }
+
+            return values;
+        }
+    }
+    class ImageControllerNiblec : ImageControllerLocal
+    {
+        protected double k;
+        protected int[,] new_pic;
+        public ImageControllerNiblec(int[,] image, int Size, double K = -0.2)
+        {
+            pic = (int[,])image.Clone();
+            new_pic = new int[pic.GetLength(0), pic.GetLength(1)];
+            size = Size;
+            k = K;
+        }
+        
+
+        protected override async Task Binaryzation()
+        {
+
+            for(int i = 0; i < pic.GetLength(0); i++)
+            {
+                for(int j = 0; j<pic.GetLength(1); j++)
+                {
+                    double math_ex = 0;
+                    double math_ex2 = 0;
+                    double dispersion = 0;
+                    List<double> values = GetValuesLocal(i, j);
+                    math_ex = values.Sum() / values.Count;
+                    math_ex2 = power(values).Sum() / values.Count;
+                    dispersion = math_ex2 - math_ex*math_ex;
+                    double locLevel = math_ex + k*Math.Sqrt(dispersion);
+                    new_pic[i,j] = pic[i, j] <= locLevel? 0 : 255;
+
+                }
+            }
+            pic = new_pic;
+        }
+    }
+    class ImageControllerSouvol : ImageControllerNiblec
+    {
+        public ImageControllerSouvol(int[,] image, int Size, double K = 0.3):base(image, Size, K)
+        {
+            pic = (int[,])image.Clone();
+            new_pic = new int[pic.GetLength(0), pic.GetLength(1)];
+            size = Size;
+            k = K;
+        }
+        protected override async Task Binaryzation()
+        {
+
+            for (int i = 0; i < pic.GetLength(0); i++)
+            {
+                for (int j = 0; j < pic.GetLength(1); j++)
+                {
+                    double math_ex = 0;
+                    double math_ex2 = 0;
+                    double dispersion = 0;
+                    List<double> values = GetValuesLocal(i, j);
+                    math_ex = values.Sum() / values.Count;
+                    math_ex2 = power(values).Sum() / values.Count;
+                    dispersion = math_ex2 - math_ex * math_ex;
+                    double locLevel = math_ex * (1+ k*(Math.Sqrt(dispersion) / 128 - 1));
+                    new_pic[i, j] = pic[i, j] <= locLevel ? 0 : 255;
+
+                }
+            }
+            pic = new_pic;
+        }
+
+    }
+
+
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ImageController con = null; 
+        
+        int[,] pixels = null;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //возвращает матрицу пикселей от pic
+        private int[,] GetMatOfPixels(Bitmap pic)
+        {
+            int[,] res = new int[pic.Width, pic.Height];
+            for (int i = 0; i < pic.Width; i++)
+                for (int j = 0; j < pic.Height; j++)
+                {
+                    System.Drawing.Color col = pic.GetPixel(i, j);
+                    double R = col.R;
+                    double G = col.G;
+                    double B = col.B;
+                    int monochromValue = (int)(0.213 * R + 0.715 * G + 0.072 * B);
+                    res[i, j] = monochromValue;
+                }
+            return res;
+        }
+        public static BitmapSource CreateBitmapSourceFromGdiBitmap(Bitmap bitmap)
+        {
+            if (bitmap == null)
+                throw new ArgumentNullException("bitmap");
+
+            var rect = new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height);
+
+            var bitmapData = bitmap.LockBits(
+                rect,
+                ImageLockMode.ReadWrite,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            try
+            {
+                var size = (rect.Width * rect.Height) * 4;
+
+                return BitmapSource.Create(
+                    bitmap.Width,
+                    bitmap.Height,
+                    bitmap.HorizontalResolution,
+                    bitmap.VerticalResolution,
+                    PixelFormats.Bgra32,
+                    null,
+                    bitmapData.Scan0,
+                    size,
+                    bitmapData.Stride);
+            }
+            finally
+            {
+                bitmap.UnlockBits(bitmapData);
+            }
+        }
+        //private void setImage(object argument)
+        //{
+        //    System.Windows.Controls.Image image = argument as System.Windows.Controls.Image;
+        //    con = new ImageControllerGav(75, pixels);
+
+        //    imageBin1.Source = con.CreateBitmapSourceFromGdiBitmap();
+        //}
+
+        private void imageBoxInput_Loaded(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.Image image = sender as System.Windows.Controls.Image;
+            Bitmap bitmap = new Bitmap("in1.jpg");
+            pixels = GetMatOfPixels(bitmap);
+            image.Source = CreateBitmapSourceFromGdiBitmap(bitmap);
+        }
+
+        private async void imageBoxInput_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            System.Windows.Controls.Image image = sender as System.Windows.Controls.Image;
+            ofd.Title = "Open Image";
+            ofd.Filter = "jpg files (*.jpg; *.jpeg)|*.jpg;*.jpeg";
+            //ParameterizedThreadStart setImageThread = new ParameterizedThreadStart(setImage);
+            //Thread thread1 = new Thread(setImageThread);
+            if ((bool)ofd.ShowDialog())
+            { 
+                Bitmap bitmap = new Bitmap(ofd.FileName);
+                pixels = GetMatOfPixels(bitmap);
+                image.Source = CreateBitmapSourceFromGdiBitmap(bitmap);
+            }
+
+            //con = new ImageControllerOtsu(pixels);
+            //imageBin1.Source = await con.CreateBitmapSourceFromGdiBitmap();
+            //con = new ImageControllerGav(125, pixels);
+            //imageBin2.Source = await con.CreateBitmapSourceFromGdiBitmap();
+            con = new ImageControllerNiblec(pixels, 9, -0.01);
+            imageBin3.Source = await con.CreateBitmapSourceFromGdiBitmap();
+            con = new ImageControllerSouvol(pixels, 9, 0.01);
+            imageBin4.Source = await con.CreateBitmapSourceFromGdiBitmap();
+        }
+    }
+}
